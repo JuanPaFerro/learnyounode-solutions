417ccd1864d130772d89b4265886348d0977b1ff
unknown <juanpabloferroarroyo@gmail.com>
2021-10-14 23:56:27 +0200

    challenge 9 successfull

---
+++ async.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const bl = require("bl");
+const urls = [process.argv[2], process.argv[3], process.argv[4]];
+
+let responses = [];
+let counter = 0;
+
+let request = (i) => {
+  http.get(urls[i], (res) => {
+    res.pipe(
+      bl((err, data) => {
+        if (err) {
+          return console.log(err);
+        }
+        data = data.toString();
+        responses[i] = data;
+        counter++;
+
+        if (counter === 3) {
+          for (let j = 0; j < responses.length; j++) {
+            console.log(responses[j]);
+          }
+        }
+      })
+    );
+  });
+};
+
+for (let i = 0; i < urls.length; i++) {
+  request(i);
+}
