e6ebad26d1daf060250f9e878be33eaf6ef43339
unknown <juanpabloferroarroyo@gmail.com>
2021-10-14 22:55:24 +0200

    implementing another way on bl-httpCollection

--- bl-httpCollection.js
+++ bl-httpCollection.js
@@ -1,14 +1,34 @@
+/*THE FIRST ONE WAS MY FIRST SOLUTION, AND IT PASSED ALL THE TESTS BUT THE OFFICIAL SOLUTION USED ".pipe()". SO I TRIED
+TO CODE IT THAT WAY AND I SUCCEED */
+
+/*
+ const http = require("http");
+ const BufferList = require("bl");
+ const { Buffer } = require("safe-buffer");
+ const url = process.argv[2];
+
+ let bl = new BufferList();
+
+ http.get(url, (res) => {
+   res.on("data", (chunk) => bl.append(new Buffer(chunk)));
+   res.on("end", () => {
+        console.log(bl.length);
+        console.log(bl.toString())
+   });
+ });
+*/
+
 const http = require("http");
-const BufferList = require("bl");
-const { Buffer } = require("safe-buffer");
+const bl = require("bl");
 const url = process.argv[2];
 
-let bl = new BufferList();
-
 http.get(url, (res) => {
-  res.on("data", (chunk) => bl.append(new Buffer(chunk)));
-  res.on("end", () => {
-       console.log(bl.length);
-       console.log(bl.toString())
-  });
+  res.pipe(bl((err, data)=>{
+    if (err) {
+      return console.log(err)
+    }
+    data = data.toString()
+    console.log(data.length)
+    console.log(data)
+  }))
 });
